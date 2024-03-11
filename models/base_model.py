e0005839f1e59aae75e1b01f3012deef3b58e1d0
Abram17 <abram.albert.aa@gmail.com>
2024-03-11 10:55:19 +0200

    startover

--- models/base_model.py
+++ models/base_model.py
@@ -1,62 +1,35 @@
 #!/usr/bin/python3
 """
-contains the BaseModel class
+conains the BaseModel class
 """
-
-from datetime import datetime
 from uuid import uuid4
-from models import storage
+from datetime import datetime
 
 
-class BaseModel():
-    """
-    defines all common attributes/methods for other classes
-    """
-
-    def __init__(self, *args, **kwargs):
+class BaseModel:
+    def __init__(self):
         """
-        initilizes BaseModel
-
-        Args:
-            _*args: arguments
-            _**kwargs: key values of args
+        initilizes the BaseModel
         """
-
-        time = "%Y-%m-%dT%H:%M:%S.%f"
-
-        if kwargs is None | kwargs == {}:
-            self.id = str(uuid4)
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            storage.new(self)
-
-        else:
-            for key, value in kwargs.items():
-                if key == "__class__":
-                    continue
-                elif key == "updated_at" | key == "created_at":
-                    setattr(self, key, datetime.strptime(value, time))
-                else:
-                    setattr(self, key, value)
+        self.id = str(uuid4)
+        self.created_at = datetime.utcnow()
+        self.updated_at = datetime.utcnow()
 
     def save(self):
         """
-        updates the public instance attribute
-        updated_at with the current datetime
+        updates updated_at with the current datetime
         """
         self.updated_at = datetime.utcnow()
-        storage.save()
 
     def to_dict(self):
         """
         returns a dictionary containing:
         all keys/values of __dict__ of the instance
         """
-
         this_dict = self.__dict__.copy()
-        this_dict["__class__"] = type(self).__name__
-        this_dict["created_at"] = this_dict["created_at"].isoformat()
-        this_dict["updated_at"] = this_dict["updated_at"].isoformat()
+        this_dict[__class__] = self.__class__.__name__
+        this_dict["created_at"] = self.created_at.isoformat()
+        this_dict["updated_at"] = self.updated_at.isoformat()
 
         return this_dict
 
@@ -64,6 +37,4 @@ class BaseModel():
         """
         print: [<class name>] (<self.id>) <self.__dict__>
         """
-
-        return "[{}] ({}) {}".\
-            format(type(self).__name__, self.id, self.__dict__)
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+contains unittests for the BaseModel
+"""
+import unittest
+from models.base_model import BaseModel
+
+
+class TestBaseModel(unittest.TestCase):
+    def Test_init(self):
+        new_model = BaseModel()
+        self.assertIsNotNone(new_model.id)
+        self.assertIsNotNone(new_model.created_at)
+        self.assertIsNotNone(new_model.updated_at)
+
+    def test_save(self):
+        new_model = BaseModel()
+        first = new_model.updated_at
+        second = new_model.save()
+        self.assertNotEqual(first, second)
+
+    def test_to_dict(self):
+        new_model = BaseModel()
+        new_model_d = new_model.to_dict()
+        self.assertIsInstance(new_model_d, dict)
+        self.assertEqual(new_model_d["__class__"], "BaseModel")
+        self.assertEqual(new_model_d["id"], new_model.id)
+        self.assertEqual(new_model_d["created_at"], new_model.created_at)
+        self.assertEqual(new_model_d["updated_at"], new_model.updated_at)
+
+    def test_str(self):
+        new_model = BaseModel()
+        self.assertTrue(str(new_model).startswith("[BaseModel]"))
+        self.assertIn(new_model.id, str(new_model))
+        self.assertIn(str(new_model.__dict__), str(new_model))
+
+
+if __name__ == "__main__":
+    unittest.main()
