7f92258df7bc5bf8448168ea91751d4c4d44aedf
Abram17 <abram.albert.aa@gmail.com>
2024-03-11 14:17:05 +0200

    task 6

--- console.py
+++ console.py
@@ -11,25 +11,30 @@ class HBNBCommand(cmd.Cmd):
     """
     prompt = "(hbnb)"
 
-    def quit_func(self, arg):
+    def do_quit(self, *args):
         """
         exit the program
         """
         return True
 
-    def EOF(self, arg):
+    def help_quit(self, *args):
+        """
+        prints a notice on how to quit
+        """
+        print("Quit command to exit the program")
+
+    def do_EOF(self, *args):
         """
         end of line
         """
         print()
         return True
 
-    def helper(self, arg):
+    def emptyline(self):
         """
-        notice to help you quit
+        empty line + ENTER: execute nothing
         """
-        print("typy quit to exit")
-
+        pass
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
