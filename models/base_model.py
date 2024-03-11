810528950accfaf6f8d58054fb45b2eb20cae806
Abram17 <abram.albert.aa@gmail.com>
2024-03-11 11:55:42 +0200

    task 4

--- models/base_model.py
+++ models/base_model.py
@@ -12,19 +12,19 @@ class BaseModel:
         initilizes the BaseModel
         """
         if kwargs:
-            for k, v in kwargs.items():
-                if k == "__class__":
-                    continue
-                elif k == "created_at" | k == "updated_at":
-                    setattr(self, k,
-                            datetime.strptime(v, "%Y-%m-%dT%H:%M:%S.%f"))
+            for k in kwargs:
+                if k == "created_at":
+                    self.__dict__["created_at"] = datetime.\
+                        strptime(kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                elif k == "updated_at":
+                    self.__dict__["updated_at"] = datetime.\
+                        strptime(kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
                 else:
-                    setattr(self, k, v)
+                    self.__dict__[k] = kwargs[k]
         else:
             self.id = str(uuid4())
             self.created_at = datetime.utcnow()
             self.updated_at = datetime.utcnow()
-
 
     def save(self):
         """
